3e6fc736a84fb9cd242fe0043ed9dffd9391a219
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-09-19 11:06:50 +0000

    set up nav example

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+app,rt = fast_app(live=true)
+
+def AppLayout(content: FT, current_path: str):
+    nav_links = [
+        {"name": "Home", "href": "/"},
+        {"name": "About", "href": "/about"},
+        {"name": "Contact Us", "href": "/contact"},
+    ]
+    return Div(
+        Nav(
+            Ul(
+                *[Li(A(nl["name"] + ("✅" if current_path == nl["href"] else ""), href=nl["href"])) for nl in nav_links]
+            )
+        ),
+        Hr(),
+        content
+    )
+
 
 @rt('/')
-def get(req): return Div(P('Hello World!'), hx_get="/change")
-
-@rt('/change')
-def get(): return P('Nice to be here!')
+def get(request): return AppLayout(content=Div("Home"), current_path=request.url.path)
+@rt('/about')
+def get(request): return AppLayout(content=Div("About"), current_path=request.url.path)
+@rt('/contact')
+def get(request): return AppLayout(content=Div("Contact us"), current_path=request.url.path)
 
 serve()
